9633ff3c96694695a8304085ede7c6749f54617d
Michael Fowler <redfaction8008@gmail.com>
2023-04-20 23:10:18 -0500

    added starting code for the project in the index.html file, CSS file and JS file added start button and timer got the timer to work and the start button to work and asked one question to the user

--- Script.js
+++ Script.js
@@ -1,1 +1,27 @@
+var timerEl = document.getElementById("timer");
+const question = document.getElementById("questions");
+const start = document.getElementById("start-btn");
+let secondsLeft = 10;
+var questions = ("What is 1+1 ?");
 
+start.addEventListener("click", function () {
+    console.log("start");
+    startQuiz();
+});
+    
+function startQuiz() {
+  document.getElementById("start-btn").style.display = "none";
+   var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timerEl.textContent = secondsLeft;
+
+      if (secondsLeft === 0) {
+          clearInterval(timerInterval);
+          console.log("Time is up!");
+      }
+
+  }, 1000);
+
+  question.innerHTML = questions;
+
+ }
