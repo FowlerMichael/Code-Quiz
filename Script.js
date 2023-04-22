a8890972abbd085d7b1712a6d77f320aeda36ac6
Michael Fowler <redfaction8008@gmail.com>
2023-04-22 12:27:20 -0500

    added high score page and added a link to it added answers buttons added css for score and highScore added html for highScore page

--- Script.js
+++ Script.js
@@ -1,16 +1,37 @@
 var timerEl = document.getElementById("timer");
-const question = document.getElementById("questions");
 const start = document.getElementById("start-btn");
+const questionsEl = document.getElementById("question");
+const answersEl = document.getElementById("answer");
 let secondsLeft = 10;
-var questions = ("What is 1+1 ?");
+let score = 0;
+
+const questions= [
+    {
+    question: "what is 1+1?",
+    answers: [ {text: "1", correct: false},
+    {text: "2", correct: true},
+    {text: "3", correct: false},
+    {text: "4", correct: false}
+    ]
+  },
+  { question: "what is 2+2?",
+    answers: [ {text: "1", correct: false},
+    {text: "2", correct: false},
+    {text: "3", correct: false},
+    {text: "4", correct: true}
+    ]
+  },
+ ];
 
 start.addEventListener("click", function () {
     console.log("start");
     startQuiz();
+    
 });
     
 function startQuiz() {
   document.getElementById("start-btn").style.display = "none";
+
    var timerInterval = setInterval(function () {
       secondsLeft--;
       timerEl.textContent = secondsLeft;
@@ -21,7 +42,21 @@ function startQuiz() {
       }
 
   }, 1000);
+}
 
-  question.innerHTML = questions;
+
+function askQuestion() {
+    questionEl.innerText = questions[0].question;
+    questions[0].answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+        button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        answerEl.appendChild(button);
+    });
+
 
  }
