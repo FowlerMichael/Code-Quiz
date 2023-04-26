a11db0b502626a77c308a74d8b28cdb565928d4a
Michael Fowler <redfaction8008@gmail.com>
2023-04-25 21:07:25 -0500

    added questions to the quiz added answers to the quiz added answer buttons to the quiz with a function to check if the answer is correct added a score counter and game over prompt added a high score page with a link to it

--- Script.js
+++ Script.js
@@ -1,62 +1,117 @@
 var timerEl = document.getElementById("timer");
+var scoreEl = document.getElementById("score");
 const start = document.getElementById("start-btn");
-const questionsEl = document.getElementById("question");
-const answersEl = document.getElementById("answer");
-let secondsLeft = 10;
+var currentQuestionIndex = 0;
+let secondsLeft = 20;
 let score = 0;
+var questionText = document.getElementById("question");
+var answer = document.getElementById("answers");
 
-const questions= [
+//var highScore = document.getElementById("highScore");
+
+//const saveHs = localStorage.setItem("highScore");
+
+
+var question= [
     {
-    question: "what is 1+1?",
-    answers: [ {text: "1", correct: false},
-    {text: "2", correct: true},
-    {text: "3", correct: false},
-    {text: "4", correct: false}
-    ]
+    question: ("what Dose HTML stand for?"),
+    answers: [ "Hyper Text Markdown Language","Hyper Text Markup Language","Hyper Text Machine Language","Hyper Text Markup Leveler"],
+    correctAnswer: "1"
   },
-  { question: "what is 2+2?",
-    answers: [ {text: "1", correct: false},
-    {text: "2", correct: false},
-    {text: "3", correct: false},
-    {text: "4", correct: true}
-    ]
+  { question: "what Dose CSS Stand for?",
+    answers: [ "cascading style sheet", "cascading style syntax", "cascading syntax sheet", "code style sheet"],
+    correctAnswer: "0"
+  },
+  { question: "what Dose JS Stand for?",
+    answers: [ "java script", "java style", "java sheet", "java syntax"],
+    correctAnswer: "0"
+  },
+  { question: "what Dose DOM Stand for?",
+    answers: [ "document object message", "document option model", "document object model", "document of main"],
+    correctAnswer: "2"
+  },
+  { question: "what Dose API Stand for?",
+    answers: [ "application program interstyle", "application program intersheet", "application page interface", "application program interface"],
+    correctAnswer: "3"
   },
  ];
 
 start.addEventListener("click", function () {
     console.log("start");
+    document.getElementById("start-btn").style.display = "none";
     startQuiz();
+    askQuestion();
     
 });
     
 function startQuiz() {
-  document.getElementById("start-btn").style.display = "none";
-
-   var timerInterval = setInterval(function () {
+  
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreEl.textContent = "Score: " + score;
+    
+  var timerInterval = setInterval(function () {
       secondsLeft--;
       timerEl.textContent = secondsLeft;
 
-      if (secondsLeft === 0) {
+      if (secondsLeft === 0 || currentQuestionIndex === question.length) {
           clearInterval(timerInterval);
-          console.log("Time is up!");
+          console.log("game over!");
+        resetGame();  
       }
 
   }, 1000);
 }
-
-
-function askQuestion() {
-    questionEl.innerText = questions[0].question;
-    questions[0].answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("btn");
-        if (answer.correct) {
-        button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-        answerEl.appendChild(button);
-    });
-
-
+function resetGame() {
+ var gameOver = confirm("Game Over! Your score is " + score + " Would you like to play again?");
+      if (gameOver === true) {
+       secondsLeft = 20;
+       currentQuestionIndex = 0;
+       startQuiz();
+       askQuestion();
+       displayAnswers(currentQuestionIndex);
+      } else {
+          alert("Thanks for playing!");
+         }
  }
+ 
+function displayAnswers(currentQuestion) {
+    for (let i = 0; i < currentQuestion.answers[i]; i++) {
+      let answerBtn = document.createElement("button");
+      answerBtn.textContent = currentQuestion.answers[i];
+      answerBtn.setAttribute("class", "answer-btn");
+      answerBtn.setAttribute("value", i);
+      answer.appendChild(answerBtn);
+    }
+  }
+  
+  function askQuestion() {
+    let currentQuestion = question[currentQuestionIndex];
+    let questionInt = currentQuestionIndex + 1;
+    questionText.textContent = questionInt + ". " + currentQuestion.question;
+    answer.innerHTML = "";
+  
+    for (let i = 0; i < currentQuestion.answers.length; i++) {
+      let answerBtn = document.createElement("button");
+      answerBtn.textContent = currentQuestion.answers[i];
+      answerBtn.setAttribute("class", "answer-btn");
+      answerBtn.setAttribute("value", i);
+      answer.appendChild(answerBtn);
+  
+      answerBtn.addEventListener("click", function () {
+        if (answerBtn.value === currentQuestion.correctAnswer) {
+          score++;
+          scoreEl.textContent = "Score: " + score;
+        } else {
+            secondsLeft -= 5;
+            
+        }
+        currentQuestionIndex++;
+        if (currentQuestionIndex < question.length) {
+          askQuestion();
+        } 
+      });
+    
+    displayAnswers(currentQuestion);
+    }
+  }
